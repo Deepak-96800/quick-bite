fa6882573c40c65293a0ab78e8e226e69b3c8f2e
Deepak-96800 <gopaldeepak490@gmail.com>
2026-07-11 17:15:59 +0530

    #

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,51 +1,16 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-
+import Navbar from "../../components/Navbar/navbar";
+import Hero from "../../components/Hero/hero";
+import Categories from "../../components/Categories/categories";
+import PopularFoods from "../../components/PopularFoods/popularfoods";
 
 function Home() {
-  const [foods, setFoods] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://quick-bite-backend-g4k9.onrender.com/foods")
-      .then((res) => setFoods(res.data))
-      .catch((err) => console.log(err));
-  }, []);
-
-const addToCart = (food) => {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-  cart.push(food);
-
-  localStorage.setItem("cart", JSON.stringify(cart));
-
-  alert("Added to cart");
-};
-  
   return (
-    <div className="container">
-      <h1 className="title">Food Menu <i class="fi fi-sr-menu-food" style={{color:"orange"}}></i></h1>
-
-      <div className="grid">
-        {foods.map((food) => (
-          <div className="card" key={food.id}>
-            <img
-              src={food.image}
-              alt={food.name}
-              className="image"
-            />
-
-            <h2>{food.name}</h2>
-            <p className="price">₹{food.price}</p>
-
-<button className="btn" onClick={() => addToCart(food)}>
-  Add to Cart <i class="fi fi-rr-shopping-cart-add"></i>
-</button>
-          </div>
-        ))}
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Categories />
+      <PopularFoods />
+    </>
   );
 }
 
