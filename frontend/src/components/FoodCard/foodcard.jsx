cd3ed2a6e54ee9a010fe3be73d2f27174d362623
Deepak-96800 <gopaldeepak490@gmail.com>
2026-07-11 16:21:38 +0530

    #

--- frontend/src/components/FoodCard/foodcard.jsx
+++ frontend/src/components/FoodCard/foodcard.jsx
@@ -1,3 +1,36 @@
+import "./foodcard.css";
+
+function FoodCard({ food, addToCart }) {
+  return (
+    <div className="food-card">
+
+      <img
+        src={food.image}
+        alt={food.name}
+      />
+
+      <div className="food-info">
+
+        <h3>{food.name}</h3>
+
+        <p className="rating">
+          ⭐ 4.8
+        </p>
+
+        <h2>₹{food.price}</h2>
+
+        <button
+          onClick={() => addToCart(food)}
+        >
+          Add to Cart
+        </button>
+
+      </div>
+
+    </div>
+  );
+}
+
 <div className="food-card">
 
     <div className="favorite">
@@ -42,3 +75,5 @@
     </div>
 
 </div>
+
+export default FoodCard;
