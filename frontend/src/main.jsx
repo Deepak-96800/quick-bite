12066fc1f1c17fdd6ba2017ef52a2e8299b71728
Deepak-96800 <gopaldeepak490@gmail.com>
2026-07-11 17:25:24 +0530

    #

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './index.css'
-import { CartContext } from "../../context/cartcontext";
+import CartProvider from "./context/cartcontext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
