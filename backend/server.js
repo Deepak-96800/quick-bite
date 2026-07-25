db7eab276b6c02245650ef9fba1eedeef4fb637b
Deepak-96800 <gopaldeepak490@gmail.com>
2026-07-25 16:13:22 +0530

    #

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,13 @@ const paymentRoutes = require("./routes/payment");
 
 const app = express();
 
+const path = require("path");
+
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "images"))
+);
+
 app.use(cors());
 app.use(express.json());
 
